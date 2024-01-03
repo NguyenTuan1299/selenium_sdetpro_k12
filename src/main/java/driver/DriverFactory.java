47b664eca5e18e237ce281ba338236c23c45afef
Tuan <anhtuantn0000@gmail.com>
2024-01-03 23:21:57 +0700

    Day 22, day 23

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -2,13 +2,17 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 public class DriverFactory {
 
     public static WebDriver getWebDriver(){
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        // This is a good workaround to not enable password manger popup from chrome
+        chromeOptions.addArguments("--incognito");
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
         /*
          * Global waiting strategy for the whole session when finding an element/elements
