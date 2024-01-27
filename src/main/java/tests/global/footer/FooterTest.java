c365c197b9c826dac2172f2f9e0d8a483d074ea1
Tuan <anhtuantn0000@gmail.com>
2024-01-27 21:45:00 +0700

    day 25

--- src/main/java/tests/global/footer/FooterTest.java
+++ src/main/java/tests/global/footer/FooterTest.java
@@ -1,5 +1,6 @@
 package tests.global.footer;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import test_flows.global.FooterTestFlow;
 import tests.BaseTest;
@@ -8,12 +9,15 @@ public class FooterTest extends BaseTest {
 
     @Test
     public void testHomePageFooter() {
+        WebDriver driver = getDriver();
         driver.get("https://demowebshop.tricentis.com/");
         FooterTestFlow footerTestFlow = new FooterTestFlow(driver);
         footerTestFlow.verifyFooterComponent();
     }
+
     @Test
     public void testCategoryPageFooter() {
+        WebDriver driver = getDriver();
         driver.get("https://demowebshop.tricentis.com/");
         FooterTestFlow footerTestFlow = new FooterTestFlow(driver);
         footerTestFlow.verifyProductCatFooterComponent();
